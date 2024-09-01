0bb3fbf51122146c90d6466d4d318cd8bed94446
Your Name <basmalaelshabrawy37@gmai.com>
2024-09-01 23:53:49 +0300

    Indexes

---
+++ EF2/Indexes/Indexes/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexes
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=EF-Index;Integrated Security=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasIndex(b => b.URL)
+                .IsUnique()
+                .HasFilter(null);
+
+          //  modelBuilder.Entity<Person>()
+           //    .HasIndex(p => new { p.FName, p.LName });
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+       // public DbSet<Person> Persons { get; set; }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+
+            public string URL { get; set; }
+
+            public List<Post> Posts { get; set; }
+        }
+        public class Post
+        {
+            public int PostId { get; set; }
+            public  int BlogId { get; set; }
+            public Blog Blog { get; set; }
+        }
+        public class Person
+        {
+            public int Id { get; set; }
+           
+            public string FName { get; set; }
+            public string LName { get; set; }
+        }
+    }
+}
---
+++ EF2/Indexes/Indexes/Migrations/20240901203314_Test2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Indexes.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_URL",
+                table: "Blogs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_URL",
+                table: "Blogs",
+                column: "URL",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_URL",
+                table: "Blogs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_URL",
+                table: "Blogs",
+                column: "URL");
+        }
+    }
+}
